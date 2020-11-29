e32c4ee3bda95c47f8360e821a4356038661c5c1
Djordje Lacmanovic <djordje.lacmanovic@gmail.com>
2020-11-29 18:54:14 +0100

    remove check_origin

--- config/prod.exs
+++ config/prod.exs
@@ -11,7 +11,6 @@ use Mix.Config
 # before starting your production server.
 config :multichess, MultichessWeb.Endpoint,
   url: [host: "${HOSTNAME}", port: 80],
-  check_origin: ["//${HOSTNAME}"],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
