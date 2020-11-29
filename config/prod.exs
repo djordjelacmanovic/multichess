7d83bc0f04efdb62c0e23e3ff1d8fe88cb040d22
Djordje Lacmanovic <djordje.lacmanovic@gmail.com>
2020-11-29 18:38:07 +0100

    actual fix to hostname

--- config/prod.exs
+++ config/prod.exs
@@ -10,7 +10,7 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :multichess, MultichessWeb.Endpoint,
-  url: [host: Application.get_env(:multichess, :hostname), port: 80],
+  url: [host: "${HOSTNAME}", port: 80],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
 # Do not print debug messages in production
